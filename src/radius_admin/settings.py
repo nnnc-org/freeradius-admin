a423aa5698f0c3968c3f4df75386631c01488db2
Cody Ernesti <cernesti@esu2.org>
2022-07-04 01:57:35 -0500

    Fix 500 errors & redis url issues

--- src/radius_admin/settings.py
+++ src/radius_admin/settings.py
@@ -225,7 +225,7 @@ HUEY = {
     'immediate': False,
     'blocking': True,  # Perform blocking pop rather than poll Redis.
     'connection': {
-        'url': os.environ.get('REDIS_HUEY_URL', 'redis://redis:6379/1'),
+        'url': os.environ.get('REDIS_URL', 'redis://redis:6379/1'),
     },
     'consumer': {
         'workers': 2,
